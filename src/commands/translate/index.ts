9d055d471494fb99617735aa1e13e6b5cc45a55b
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-11-28 09:05:22 +0700

    add : translate feature

---
+++ src/commands/translate/index.ts
@@ -0,0 +1,33 @@
+import { Command } from "discord.js"
+import translate from '@vitalets/google-translate-api'
+
+const command: Command = {
+    name: "translate",
+    description: "translate lang",
+    async execute(message, args) {
+        const [lang, ...words]: Array<string> = args
+        const word = words.join(' ')
+        translate(word, { to: lang }).then(res => {
+            const embedMessage = {
+                "fields": [
+                    {
+                        "name": `From ${res.from.language.iso}`,
+                        "value": word,
+                        "inline": false
+                    },
+                    {
+                        "name": `To ${lang}`,
+                        "value": res.text,
+                        "inline": false
+                    }
+                ]
+            }
+            console.log(res)
+            message.channel.send({ embeds: [embedMessage] });
+        }).catch(err => {
+            console.error(err);
+        });
+    },
+};
+
+export = command;
